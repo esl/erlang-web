bd4e53c6f6a35ed8bf98b5b3d7f870d2c5d4865f
michal.ptaszek <none@none>
2009-03-04 14:20:25 +0000

    e_annotation.hrl added, changed start.erl script to accept additional attributes
    --HG--
    branch : eptic-1.3
    extra : convert_revision : svn%3A9efba7b2-c818-0410-9093-eca7794ad90f/branches/eptic-1.3%40960

---
+++ include/e_annotation.hrl
@@ -0,0 +1,15 @@
+-define(INVALIDATE(Regexps), 
+	-invalidate(Regexps)).
+
+-define(INVALIDATE_IF(Regexps, Pred),
+	-invalidate(Regexps, Pred)).
+
+-define(INVALIDATE_GROUPS(Groups),
+	-invalidate_groups(Groups)).
+
+-define(INVALIDATE_GROUPS_IF(Groups, Pred),
+	-invalidate_groups(Groups, Pred)).
+
+-define(BACKEND_CALL,
+	-backend_call([])).
+	
