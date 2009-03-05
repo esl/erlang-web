f32771b77131d9ce68c9f8387f5765795d892fee
michal.ptaszek <none@none>
2009-03-05 15:24:13 +0000

    e_annotation is now compiled in the first row
    --HG--
    branch : erlangweb-1.3
    extra : convert_revision : svn%3A9efba7b2-c818-0410-9093-eca7794ad90f/branches/erlangweb-1.3%40995

--- bin/compile.erl
+++ bin/compile.erl
@@ -1,5 +1,28 @@
 #!/usr/bin/env escript
 
+%% The contents of this file are subject to the Erlang Web Public License,
+%% Version 1.0, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Web Public License along with this software. If not, it can be
+%% retrieved via the world wide web at http://www.erlang-consulting.com/.
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% The Initial Developer of the Original Code is Erlang Training & Consulting
+%% Ltd. Portions created by Erlang Training & Consulting Ltd are Copyright 2009,
+%% Erlang Training & Consulting Ltd. All Rights Reserved.
+
+%%%-----------------------------------------------------------------------------
+%%% File    : start.erl
+%%% @author Michal Ptaszek <michal.ptaszek@erlang.consulting.com>
+%%% @doc Compile tool for Erlang Web framework.
+%%% It deals with release handling as well.
+%%% @end
+%%%-----------------------------------------------------------------------------
+
 main([]) ->
     make_basic();
 main(["release", Vsn, "yaws"]) ->
@@ -23,12 +46,22 @@ make_basic() ->
     {ok, Dir} = file:list_dir("lib/"),
     [code:add_path("lib/" ++ D ++ "/ebin") || D <- Dir],
 
+    compile_e_annotation(),
+
     case make:all() of
 	up_to_date ->
 	    io:format("Compilation completed!~n");
 	error ->
 	    io:format("There were errors during the compilation!~n")
     end.
+
+compile_e_annotation() ->
+    application:load(eptic),
+    LibDir = code:lib_dir(eptic),
+
+    compile:file(filename:join([LibDir, "src", "e_annotation.erl"]),
+		 [debug_info,
+		  {outdir, filename:join([LibDir, "ebin"])}]).
 
 make_release(RelVsn, Server) ->
     make_basic(),
