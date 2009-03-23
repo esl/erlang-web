b021fdd9de78b4dc8d411f3324388a869daf3633
michal.ptaszek <none@none>
2009-03-23 10:43:12 +0000

    wpartlib restored
    --HG--
    branch : wpart-1.3
    extra : convert_revision : svn%3A9efba7b2-c818-0410-9093-eca7794ad90f/branches/wpart-1.3%401066

--- src/wpartlib.erl
+++ src/wpartlib.erl
@@ -10,17 +10,18 @@
 %% under the License.
 %%
 %% The Initial Developer of the Original Code is Erlang Training & Consulting
-%% Ltd. Portions created by Erlang Training & Consulting Ltd are Copyright 2009,
+%% Ltd. Portions created by Erlang Training & Consulting Ltd are Copyright 2008,
 %% Erlang Training & Consulting Ltd. All Rights Reserved.
 
 %%%-------------------------------------------------------------------
 %%% File    : wpartlib.erl
-%%% @author Michal Ptaszek <michal.ptaszek@erlang-consulting.com>
-%%% @doc An interface module to the engine-specific template expander.
-%%% @end
+%%% Author  : Martin <info@erlang-consulting.com>
+%%% Description : 
+%%%
 %%%-------------------------------------------------------------------
-
 -module(wpartlib).
+
+%% API
 -export([select/2,
 	 has_attribute/2, 
 	 attribute/3,
@@ -30,54 +31,125 @@
          eval_file/2,
          expand_string/1,
 	 search/2,
+	 search/1,
 	 decode_erl/1
 	]).
 
-%% @see wpart:select/2
--spec(select/2 :: (string(), term()) -> term()).
-select(Path, XML) ->
-    route(select, [Path, XML]).
+-include_lib("xmerl/include/xmerl.hrl").
 
-%% @see wpart:has_attribute/2
--spec(has_attribute/2 :: (string(), term()) -> false | string()).	     
-has_attribute(Path, XML) ->
-    route(has_attribute, [Path, XML]).
+%%====================================================================
+%% API
+%%====================================================================
+select(Path, #xmlElement{} = E) ->
+    xmerl_xs:select(Path, E).
+	    
+has_attribute(Path, #xmlElement{} = E) ->
+    case xmerl_xs:select(Path, E) of
+	[#xmlAttribute{value = Value}] ->
+	    Value;
+	[] ->
+	    false
+    end.
+    
+attribute(Path, #xmlElement{} = E) ->
+    case xmerl_xs:select(Path, E) of
+	[#xmlAttribute{value = Value}] ->
+	    Value;
+	[] ->
+	    erlang:error({Path, not_set})
+    end.
 
-%%
-%% @spec attribute(XPath :: string(), XMLElement :: tuple()) -> Value :: string()
-%% @doc Retrives the attribute value from the XML element.
-%% When the wanted parameter is not present, <i>erlang:error/1</i>
-%% is called.
-%% 
--spec(attribute/2 :: (string(), term()) -> string() | none()).
-attribute(Path, XML) ->
-    route(attribute, [Path, XML]).
+attribute(Path, Default, E) ->
+    case xmerl_xs:select(Path, E) of
+	[#xmlAttribute{value = Value}] ->
+	    Value;
+	[] ->
+	    Default
+    end.
 
-%% @see wpart:has_attribute/3
--spec(attribute/3 :: (string(), term(), term()) -> term()).	     
-attribute(Path, Default, XML) ->
-    route(attribute, [Path, Default, XML]).
+format(Value, E) ->
+    case has_attribute("attribute::format", E) of
+        false ->
+            Value;
+        Format ->
+            wtype:format(Format, Value)
+    end.
 
-%% @see wpart:format/2
--spec(format/2 :: (term(), term()) -> term()).	     
-format(Value, XML) ->
-    route(format, [Value, XML]).
+search(Value) ->
+    search(Value, []).
 
-%% @hidden
--spec(search/2 :: (term(), list()) -> term()).	     
-search(Value, Tokens) ->
-    route(search, [Value, Tokens]).
+search(Value, []) ->
+    Value;
+search(Value, [Token|Tokens]) when is_list(Value) ->
+    case lists:keysearch(Token, 1, Value) of
+	{value, {Token, NewValue}} ->
+	    search(NewValue, Tokens);
+	false ->
+	    undefined
+    end;
+search(_,_) ->
+    undefined.
 
-%% @see wpart:eval/1
--spec(eval/1 :: (tuple() | list(tuple())) -> tuple() | list(tuple())).
-eval(Element) ->
-    route(eval, [Element]).
+eval_file(File, XPath) ->
+    eval(xmerl_xs:select(XPath, eptic:read_file(File))).
 
--spec(expand_string/1 :: (string()) -> string()).	     
-expand_string(String) ->
-    route(expand_string, [String]).
+%%====================================================================
+%% Internal functions
+%%====================================================================
+eval(#xmlElement{nsinfo = {"wpart", Operator}} = E) ->
+    Mod = list_to_atom("wpart_" ++ Operator),
+    Attr = expand_attributes(E#xmlElement.attributes),
+    apply(Mod, handle_call, [E#xmlElement{attributes = Attr}]);
+eval(#xmlElement{attributes = Attr, content = []} = E) ->
+    E#xmlElement{attributes = expand_attributes(Attr)};
+eval(#xmlElement{attributes = Attr} = E) ->
+    E#xmlElement{attributes = expand_attributes(Attr),
+                 content = eval(E#xmlElement.content)};
+eval([H|T]) ->
+    [eval(H)|eval(T)]; 
+eval(E) ->
+    E.
 
--spec(decode_erl/1 :: (string()) -> string()).	     
+expand_attributes([]) ->
+    [];
+expand_attributes([#xmlAttribute{namespace = {"wpart", N}, value = V}|T]) ->
+    [#xmlAttribute{name = list_to_atom(N), 
+		   namespace = [], 
+		   nsinfo = [],
+		   value = expand_string(V)}|
+     expand_attributes(T)];
+expand_attributes([A|T]) ->
+    [A|expand_attributes(T)].
+
+expand_string(S) ->
+    expand_string(S, [], [[]]).
+
+expand_string([], _, [Acc0|Acc]) ->
+    lists:flatten(lists:reverse([lists:reverse(Acc0)|Acc]));
+expand_string([${, H|T], _, [Acc0|Acc]) ->
+    expand_string(T, [H], [lists:reverse(Acc0)|Acc]);
+expand_string([$}|T], Op0, Acc) ->
+    Value = case string:tokens(lists:reverse(Op0), "[]") of
+		[Format, Key] ->
+		    [Token|Tokens] = string:tokens(Key,":"),
+		    wtype:format("[" ++ Format ++ "]", search(wpart:fget(Token), Tokens));
+		[Key] ->
+		    [Token|Tokens] = string:tokens(Key,":"),
+		    case search(wpart:fget(Token), Tokens) of
+			[] -> "[]";
+			V -> io_lib:format("~p",[V])
+		    end
+	    end,
+    expand_string(T, [], [[], Value |Acc]);
+expand_string([H|T], [], [Acc0|Acc]) ->
+    expand_string(T, [], [[H|Acc0]|Acc]);
+expand_string([H|T], Acc0, Acc) ->
+    expand_string(T, [H|Acc0], Acc).
+
+substitute_erl(Erl, From, To) ->
+    {ok, NewErl, _} = regexp:gsub(Erl, From, To),
+    NewErl.
+
 decode_erl(Code) ->
     lists:foldl(fun({From, To}, Erl) ->
 			substitute_erl(Erl, From, To)
@@ -87,26 +159,3 @@ decode_erl(Code) ->
 			    {" neq ", " =/= "},
 			    {" le ", " =< "},
 			    {" ge ", " >= "}]).
-
-%% @see wpart:eval_file/2
--spec(eval_file/2 :: (string(), string()) -> list(string()) | string()).	     
-eval_file(File, XPath) ->
-    route(eval_file, [File, XPath]).
-
-%%====================================================================
-%% Internal functions
-%%====================================================================
--spec(route/2 :: (atom(), list()) -> term()).
-route(Fun, Attrs) ->
-    Mod = case application:get_env(eptic, template_expander) of
-	      {ok, wpart_xs} ->
-		  wpartlib_xmerl
-	  end,
-
-    apply(Mod, Fun, Attrs).
-
--spec(substitute_erl/3 :: (string(), string(), string()) -> string()).	     
-substitute_erl(Erl, From, To) ->
-    {ok, NewErl, _} = regexp:gsub(Erl, From, To),
-    NewErl.
-
