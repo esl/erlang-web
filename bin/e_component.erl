8148247e3d062e27d9ad415f5858370fd6e24e79
michal.ptaszek <none@none>
2009-04-14 08:44:29 +0200

    bin/e_component.erl modifies now the root emakefile
    --HG--
    branch : eptic-1.3

--- bin/e_component.erl
+++ bin/e_component.erl
@@ -1,5 +1,27 @@
 #!/usr/bin/env escript
 
+%% The contents of this file are subject to the Erlang Web Public License,
+%% Version 1.0, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Web Public License along with this software. If not, it can be
+%% retrieved via the world wide web at http://www.erlang-consulting.com/.
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% The Initial Developer of the Original Code is Erlang Training & Consulting
+%% Ltd. Portions created by Erlang Training & Consulting Ltd are Copyright 2009,
+%% Erlang Training & Consulting Ltd. All Rights Reserved.
+
+%%%-----------------------------------------------------------------------------
+%%% File    : e_component.erl
+%%% @author Michal Ptaszek <michal.ptaszek@erlang.consulting.com>
+%%% @doc Script for downloading and installing the e_components for Erlang Web framework.
+%%% @end
+%%%-----------------------------------------------------------------------------
+
 main(Args) ->
     inets:start(),
     action(Args).
@@ -32,6 +54,8 @@ action(["path", Path, "install", Name]) ->
 		     verbose,
 		     compressed,
 		     keep_old_files]),
+
+    modify_emakefile(Path, Name),
     
     io:format("~s e_component installed successfully~n", [Name]);
 action(_) ->
@@ -69,3 +93,32 @@ make_request(Type) ->
 		      [Type, Reason]),
 	    []
     end.
+
+modify_emakefile(Path, Name) ->
+    case file:consult(filename:join([Path, Name, "Emakefile"])) of
+	{ok, Entries} ->
+	    case file:open(filename:join([Path, "..", "Emakefile"]), [append]) of
+		{ok, Fd} ->
+		    lists:map(fun(Entry) ->
+				      io:format(Fd, "~p.~n~n", [prepare_entry(filename:join([Path, Name]), Entry)])
+			      end, Entries),
+		    file:close(Fd);
+		{error, Reason} ->
+		    error_logger:warning_msg("Error during modifying the root Emakefile, reason: ~p~n",
+					     [Reason])
+	    end;
+	{error, Reason} ->
+	    error_logger:warning_msg("Error during reading the e_component's Emakefile, reason: ~p~n",
+				     [Reason])
+    end.
+
+prepare_entry(Path, {Src, Opts}) ->
+    {filename:join([Path, Src]), 
+     lists:map(fun(Opt) ->
+		       prepare_entry_opt(Path, Opt)
+	       end, Opts)}.
+
+prepare_entry_opt(Path, {Type, In}) ->
+    {Type, filename:join([Path, In])};
+prepare_entry_opt(_, Else) ->
+    Else.
