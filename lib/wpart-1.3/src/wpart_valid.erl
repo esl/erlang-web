d44acab7012d3612b5b649043bfe708255009ddb
michal.zajda <none@none>
2009-04-27 10:43:07 +0200

    new wpart_valid and missing clause in wtype_string
    --HG--
    branch : eptic-1.3

--- lib/wpart-1.3/src/wpart_valid.erl
+++ lib/wpart-1.3/src/wpart_valid.erl
@@ -1,115 +1,121 @@
-%% The contents of this file are subject to the Erlang Web Public License,
-%% Version 1.0, (the "License"); you may not use this file except in
-%% compliance with the License. You should have received a copy of the
-%% Erlang Web Public License along with this software. If not, it can be
-%% retrieved via the world wide web at http://www.erlang-consulting.com/.
-%%
-%% Software distributed under the License is distributed on an "AS IS"
-%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-%% the License for the specific language governing rights and limitations
-%% under the License.
-%%
-%% The Initial Developer of the Original Code is Erlang Training & Consulting
-%% Ltd. Portions created by Erlang Training & Consulting Ltd are Copyright 2008,
-%% Erlang Training & Consulting Ltd. All Rights Reserved.
-
-%%%-------------------------------------------------------------------
-%%% File    : wpart_valid.erl
-%%% Author  : Michal Zajda <michal.zajda@erlang-consulting.com>
-%%% Description : 
-%%%
-%%%-------------------------------------------------------------------
 -module(wpart_valid).
--export([validate/1, validate/2, validate/3, is_private/1]).
+-export([validate/1, is_private/1]).
 
-part(0,_,_,_,BasicFields,ComplexTypes, BasicTypes) ->
-    {BasicFields, ComplexTypes, BasicTypes};
-part(N, Fields, TypesList, BTList, BasicFields, ComplexTypes, BasicTypes) -> 
-    [H1|T1] = TypesList,
-    [H2|T2] = Fields,
-    {X,_} = H1,
-    Res = lists:member(X, BTList),
-    if Res -> part(N-1,T2,T1,BTList,
-		   [H2 | BasicFields],
-		   ComplexTypes, [H1 | BasicTypes]);
-       true -> part(N-1,T2,T1,BTList,BasicFields,[H1 | ComplexTypes], BasicTypes)
-    end.
 
-%% From : ["news","person"]
-validate(Fields, Types, From) ->
-    TypesList = tuple_to_list(Types),
-    [_ | TypesListTail] = TypesList,
+%% @doc the initial call from a controller
+-spec(validate/1 :: (atom()) -> {ok, list()} | {error, list()}).
+validate(TypeName) ->
+    POST = eptic:fget("post"),
+    Primitives =  e_conf:primitive_types(),
 
-    {BasicFields, Complex, Basic} = part(length(TypesList)-1, Fields, 
-					 TypesListTail, e_conf:primitive_types(),[],[],[]),
-    LongBasicFields = lists:map(fun(X)-> string:join(From, "_") ++ 
-					     "_" ++ atom_to_list(X) end, 
-				BasicFields),
-
-    LeafResult = if 
-		     Complex =/= [] -> 
-			 [[_ | Val]] = lists:map(fun({X,_}) -> 
-							 validate(X, From) 
-						 end,
-						 Complex),
-			 Val;
-		     true -> []
-		 end,
-
-    Fun = fun({{collection, _}, X}) -> 
-		  get_collection_fields(X);
-	     ({_, X}) ->
-		  eptic:fget("post", X)
-	  end,
-    PostInput = lists:map(Fun, lists:zip(Basic, LongBasicFields)),
-
-    Final = tuple_to_list(pusher(PostInput, Basic, LongBasicFields)),
-
-    Final ++ LeafResult.
-
-validate(Name) when is_atom(Name) ->
-    [_ | R] = apply(list_to_atom("wtype_" ++ atom_to_list(Name)), 
-			 validate, [[]]),
-    Result = lists:flatten(R),
-    Temp = lists:map(fun({{X,_},_})-> X end, Result),
-    
-    Bad = lists:member(error, Temp),
-    if not Bad -> {ok, Result};
-       Bad -> {error, Result}
-    end.
- 
-validate(Name, From) when is_atom(Name) ->
-    apply(list_to_atom("wtype_" ++ atom_to_list(Name)), validate, [From]). 
-
-%% BasicType - {date, [{format, YYYY-MM-DD}]}
-pusher(Input, BasicTypes, LongBasicFields) ->
-    Pairs = lists:zip(BasicTypes, Input),
-    Result = lists:map(fun check/1, Pairs),
-    save_errors(LongBasicFields, Result),
-    Bad = lists:keymember(error,1,Result),
+    {ErrorCount, V_Result} = validate(POST, Primitives, 
+				      [{atom_to_list(TypeName), TypeName}], [], 0),
+     
     if   
-	not Bad -> {ok, show_output(Result,LongBasicFields,[])};
-	true -> {error, show_output(Result,LongBasicFields,[])}
+	ErrorCount > 0 -> {error,V_Result};
+	true -> {ok, V_Result}
     end.
 
-show_output([],_,R) ->
-    R;
-show_output(Result,LongBasicFields, Output) ->
-   [H|T] = Result,
-   [HH|TT] = LongBasicFields,  
-   show_output(T,TT, Output ++ [{H, HH}]).
+%% @doc Function validates field by field from a record definition, when field aprears 
+%% to be nested type its predessor is formed and passed with TypeName
+validate(_, _, [[]], Acc, ErrorCount) ->
+    {ErrorCount,Acc};
+%% clause to match call when POST is empty (form building)
+validate(undefined, Primitives, [{Predessor, TypeName} | MoreTypes], Acc, ErrorCount) ->
+    validate([], Primitives, [{Predessor, TypeName} | MoreTypes], Acc, ErrorCount);
+validate(POST, Primitives, [{Predessor, TypeName} | MoreTypes], Acc, ErrorCount) ->
 
-%% helper function to check if input has good type (using called wtype_ )
-%% takes structure eqivalent to types record and input string 
-%% check ( { {date, [{format, YYYY-MM-DD}]} , input_string } )
-%% returns proper input (if oryginal one was not complete) {ok, StrOk} or {error, Msg}
-check({Type, Input}) ->
-    {H,T} = Type, 
-    apply(list_to_atom("wtype_" ++ atom_to_list(H)),validate,[{T,Input}]).
+    %% {[atom()],tuple()}
+    {RecInfo, TypeRecInstance} = get_record_info(TypeName),
 
-%% function which checks if the field should be validated
-%% if it is a private field - not shown on forms - it will be skipped
-%% beacuse it will be set by some default value later (e.g. ID)
+    [_ | Types] = tuple_to_list(TypeRecInstance),
+
+    {groups, PostNames, BasicTypes, ComplexFields} = 
+	sort_out(RecInfo,Types, Primitives, Predessor),
+    
+    Input = zip_get(BasicTypes, PostNames, POST, []),
+    
+    Result = validate_local(Input, BasicTypes),
+    
+    %% posible to optimize and combine with validate_local
+    Errors = save_errors(PostNames, Result),
+    ResultExt = lists:zip(Result,PostNames),
+
+    validate(POST, Primitives, [ComplexFields|MoreTypes], 
+	     ResultExt ++ Acc, ErrorCount + Errors).
+
+validate_local(L1,L2) ->
+    validate_local(L1,L2,[]).
+
+validate_local([],[],Acc) ->
+    Acc;
+validate_local([In|InputTail], [{Name,Attr} | BasicTypesTail], Acc) ->
+    Result = apply(list_to_atom("wtype_" ++ atom_to_list(Name)),validate,[{Attr,In}]),
+    validate_local(InputTail,BasicTypesTail,[Result|Acc]).
+
+%% @doc function retrieves form post single value or set of values.
+%% It should not be unified because only collection fields could have multipule vals under one key;
+%% more than one val in one-POST-er field is hack or bug and should not be propagated.
+zip_get([],[],_,Acc)->
+    Acc;
+zip_get([{collection,_}|T1],[H2|T2], POST, Acc) ->    
+    In = proplists:get_all_values(H2,POST),
+    zip_get(T1,T2,POST,[In|Acc]);
+zip_get([_|T1],[H2|T2], POST, Acc) ->
+    In = proplists:get_value(H2,POST),
+    zip_get(T1,T2,POST,[In|Acc]).
+
+%% @doc divides fields into groups to discover which fields' names are ready to retrieve from POST.
+%% It is not obvious, because types can be nested and have hierarchical names.
+%% Basic types fields are resolved to proper names (predessor name added with '_').
+%% To complex type current type is passed to become predessor.
+sort_out(RecInfo, Types, Primitives, Before) ->
+    sort_out(RecInfo, Types, Primitives, Before,[],[],[]).
+
+sort_out([], _, _, _,
+	 BasicFields, BasicTypes, ComplexFields) ->
+    {groups,  BasicFields, BasicTypes, ComplexFields};
+sort_out([H1|T1], [H2={Type, _AttrList}|T2], Primitives, Before,
+	 BasicFields, BasicTypes, ComplexFields) ->
+    case lists:member(Type, Primitives) of
+	true ->
+	    sort_out(T1,T2,Primitives,Before,
+		     [string:join([Before,atom_to_list(H1)],"_")|BasicFields],
+		     [H2|BasicTypes],ComplexFields);
+	false ->
+	    sort_out(T1,T2,Primitives,Before,
+		     BasicFields,BasicTypes,
+		     [{string:join([Before,atom_to_list(H1)],"_"), H1}|ComplexFields])
+    end.
+
+%% @doc calls a custom wtype modules to get a custom type definition
+get_record_info(TypeName) ->
+    %% TypeNameStr can be different from Predessor
+    TypeNameStr = atom_to_list(TypeName),
+    Module = list_to_atom("wtype_" ++ TypeNameStr),
+    RecordInfo = apply(Module,
+		       get_record_info,
+		       [TypeName]),
+    RecName = list_to_atom(TypeNameStr ++ "_types"),
+    TypeRecordInstance = apply(Module,
+			       get_record_info,
+			       [RecName]),
+    {RecordInfo, TypeRecordInstance}.
+
+
+%-spec(save_errors/2 :: (list(string()), list()) -> ok).	     
+save_errors(L1,L2) ->
+    save_errors(L1,L2,0).
+
+save_errors([], [], Err) ->
+    Err;
+save_errors([FieldName | RestFields], [{error, Reason} | RestResults], Err) ->
+    e_error:save(FieldName, Reason),
+    save_errors(RestFields, RestResults, Err+1);
+save_errors([_ | RestFields], [_ | RestResults], Err) ->
+    save_errors(RestFields, RestResults, Err).
+
+%% Private fields are not displayed and validated
 is_private(Params) ->
     case lists:keysearch(private, 1, Params) of
 	{value, {private, Val}} ->
@@ -117,22 +123,3 @@ is_private(Params) ->
 	false -> 
 	    false
     end.
-
-get_collection_fields(Name) ->
-    lists:foldl(fun({Key, Val}, Acc) ->
-			case string:str(Key, Name) of
-			    0 ->
-				Acc;
-			    _ ->
-				[{Key, Val} | Acc]
-			end
-		end, [], eptic:fget("post")).
-
--spec(save_errors/2 :: (list(string()), list()) -> ok).	     
-save_errors([], []) ->
-    ok;
-save_errors([FieldName | RestFields], [{error, Reason} | RestResults]) ->
-    e_error:save(FieldName, Reason),
-    save_errors(RestFields, RestResults);
-save_errors([_ | RestFields], [_ | RestResults]) ->
-    save_errors(RestFields, RestResults).
