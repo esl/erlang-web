e48d61af5e79d914e49eebbff21b7663610baca5
michal.ptaszek <none@none>
2009-04-28 15:18:57 +0200

    bin/generate.erl works now for models as well
    --HG--
    branch : eptic-1.3

--- bin/generate.erl
+++ bin/generate.erl
@@ -64,16 +64,56 @@ controller(Args0) ->
 	    case file:open(SrcFile, [write]) of
 		{ok, Fd} ->
 		    create_controller(Fd, Name, Funs),
-		    add_controller_to_app(App, Name);
+		    add_module_to_app(App, Name),
+		    io:format("File ~s created successfully!~n", [SrcFile]);
 		{error, Reason} ->
 		    io:format("Error during opening ~s for writing, reason: ~p~n", 
 			      [SrcFile, Reason])
 	    end
     end.
 
-model(_Args) ->
-    ok.
+model(Args0) ->
+    prepare_path(),
+    Args = parse_args(Args0, []),
 
+    App = list_to_atom(case proplists:get_value(app, Args) of
+			   undefined ->
+			       string:strip(io:get_line("Name of the application: "), both, 10);
+			   ValA ->
+			       ValA
+		       end),
+
+    ModelName = case proplists:get_value(name, Args) of
+		    undefined ->
+			string:strip(io:get_line("Name of the model: "), both, 10);
+		    ValN ->
+			ValN
+		end,
+    
+    Hrl = case proplists:get_value(hrl, Args) of
+	      undefined ->
+		  filename:join(["..", ModelName ++ ".hrl"]);
+	      ValH ->
+		  ValH
+	  end,
+    
+    SrcFile = filename:join([code:lib_dir(App, src), "wtype_" ++ ModelName ++ ".erl"]),
+    case filelib:is_file(SrcFile) of
+	true ->
+	    io:format("File ~s exists, skipping.~n", [SrcFile]);
+	false ->
+	    case file:open(SrcFile, [write]) of
+		{ok, Fd} ->
+		    create_model(Fd, ModelName, Hrl),
+		    add_module_to_app(App, list_to_atom("wtype_" ++ ModelName)),
+		    io:format("File ~s created successfully!~n", [SrcFile]);
+		{error, Reason} ->
+		    io:format("Error during opening ~s for writing, reason: ~p~n", 
+			      [SrcFile, Reason])
+	    end
+    end.
+
+-spec(create_controller/3 :: (pid(), atom(), list(string())) -> ok | {error, term()}).	     
 create_controller(Fd, Name, Funs) ->
     io:format(Fd, "-module(~p).~n"
 	      "-export([",
@@ -95,7 +135,52 @@ controller_function(Fd, [Fun | Rest]) ->
 controller_function(_, []) ->
     ok.
 
-add_controller_to_app(AppName, Name) ->
+-spec(create_model/3 :: (pid(), string(), string()) -> ok | {error, term()}).	     
+create_model(Fd, Name, Hrl) ->
+    VarName = [string:to_upper(hd(Name)) | tl(Name)],
+
+    io:format(Fd, "-module(wtype_~s).~n"
+	      "-export([get_record_info/1]).~n"
+	      "-export([create/1, read/1, update/1, delete/1]).~n"
+	      "-export([prepare_initial/0, prepare_validated/0]).~n~n"
+	      "-include(~p).~n~n",
+	      [Name, Hrl]),
+
+    io:format(Fd, "get_record_info(~s) -> record_info(fields, ~s);~n"
+	      "get_record_info(~s_types) -> #~s_types{}.~n~n",
+	      [Name, Name, Name, Name]),
+
+    io:format(Fd, "create(~s) ->~n"
+	      "    e_db:write(~s, ~s).~n~n",
+	      [VarName, Name, VarName]),
+    
+    io:format(Fd, "read(all) ->~n"
+	      "    e_db:read(~s);~n"
+	      "read(Id) ->~n"
+	      "    e_db:read(~s, Id).~n~n",
+	      [Name, Name]),
+
+    io:format(Fd, "update(~s) ->~n"
+	      "    e_db:update(~s, ~s).~n~n",
+	      [VarName, Name, VarName]),
+
+    io:format(Fd, "delete(Id) ->~n"
+	      "    e_db:delete(~s, Id).~n~n",
+	      [Name]),
+
+    io:format(Fd, "prepare_initial() ->~n"
+	      "    wpart_db:build_record_structure(~s, #~s{}).~n~n",
+	      [Name, Name]),
+    
+    io:format(Fd, "prepare_validated() ->~n"
+	      "    ~s = wpart:fget(\"__not_validated\"),~n"
+	      "    wpart_db:build_record_structure(~s, ~s).~n~n",
+	      [VarName, Name, VarName]),
+    
+    file:close(Fd).
+
+-spec(add_module_to_app/2 :: (atom(), atom()) -> ok | {error, term()}).	     
+add_module_to_app(AppName, Name) ->
     AppFile = filename:join([code:lib_dir(AppName, ebin)], atom_to_list(AppName) ++ ".app"),
     case file:consult(AppFile) of
 	{ok, [{application, AppName, Opts}]} ->
