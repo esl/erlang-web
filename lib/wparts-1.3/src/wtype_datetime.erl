17f52502b4e9669bfe43e0304a6724db4fc473d6
Xingdong Bian <xingdong.bian@erlang-consulting.com>
2009-08-21 12:21:26 +0100

    corrected typos in wtype_datetime - November and February

--- lib/wparts-1.3/src/wtype_datetime.erl
+++ lib/wparts-1.3/src/wtype_datetime.erl
@@ -166,7 +166,7 @@ smonth(11) -> "Nov";
 smonth(12) -> "Dec".
 
 month(1) -> "January";
-month(2) -> "Febuary";
+month(2) -> "February";
 month(3) -> "March";
 month(4) -> "April";
 month(5) -> "May";
@@ -175,7 +175,7 @@ month(7) -> "July";
 month(8) -> "August";
 month(9) -> "September";
 month(10) -> "October";
-month(11) -> "Novmber";
+month(11) -> "November";
 month(12) -> "December".
 
 sday(1) -> "Mon";
