4d371dcd613fc3ada8edc661546339831ca7c69b
Michal Ptaszek <none@none>
2009-10-19 09:12:13 +0200

    License added

--- lib/ewts-1.0/src/e_mod_ewts.erl
+++ lib/ewts-1.0/src/e_mod_ewts.erl
@@ -1,3 +1,25 @@
+%% The contents of this file are subject to the Erlang Web Public License,
+%% Version 1.0, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Web Public License along with this software. If not, it can be
+%% retrieved via the world wide web at http://www.erlang-consulting.com/.
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% The Initial Developer of the Original Code is Erlang Training & Consulting
+%% Ltd. Portions created by Erlang Training & Consulting Ltd are Copyright 2009,
+%% Erlang Training & Consulting Ltd. All Rights Reserved.
+
+%%%-------------------------------------------------------------------
+%%% File    : e_mod_ewts.erl
+%%% Author  : Michal Ptaszek <michal.ptaszek@erlang-consulting.com>
+%%% Description : Browser's EWTS layer for Erlang Web. 
+%%%
+%%% Created : 19 Oct 2009 by michalptaszek <michalptaszek@coltrane.erlangsystems.com>
+%%%-------------------------------------------------------------------
 -module(e_mod_ewts).
 
 -export([handle_request/1]).
